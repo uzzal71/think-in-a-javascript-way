f26b353b74d4dc2b82a67e8a7f85dfc34416dd81
uzzal71 <uzzalroy.acm@gmail.com>
2024-03-17 23:48:30 +0600

    Hoisting

--- 4.hoisting/hoisting.js
+++ 4.hoisting/hoisting.js
@@ -3,6 +3,21 @@
  * var vs let (and const)
  * function
  */
-var a;
-console.log(a);
-a = "Bangladesh";
+
+let LANGUAGE = "Java";
+let language = "javaScript";
+
+function getLanguage() {
+  if (!language) {
+    let language = LANGUAGE;
+  }
+  return language;
+}
+
+// console.log(`I love ${getLanguage()}`);
+
+myFunc();
+
+function myFunc() {
+  console.log(`I love JavaScript`);
+}
