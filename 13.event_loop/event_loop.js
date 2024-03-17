3929b8e806433eaee63395faed15aad2c2d76758
uzzal71 <uzzalroy.acm@gmail.com>
2024-03-18 00:59:03 +0600

    Event loop

--- 13.event_loop/event_loop.js
+++ 13.event_loop/event_loop.js
@@ -3,8 +3,15 @@ while (shouldIRun()) {
 }
 
 /**
- * shouldIRun
+ * shouldIRun() how to working...
  * @ setTimeout, setInterval etc.
  * @ OS Tasks? (HTTP server)
  * @ Own Tasks? (Filesystem)
+ */
+
+/**
+ * doSomeThingAmazing() how to working...
+ * Pending timers done? - run callback
+ * Pending OS/own task done? - run callback
+ * Resume the event loop
  */
