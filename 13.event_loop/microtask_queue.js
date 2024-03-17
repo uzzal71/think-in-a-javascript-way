aca60034c7262ad8aedc638d3612103379bd4776
uzzal71 <uzzalroy.acm@gmail.com>
2024-03-18 00:53:03 +0600

    Event loop

--- 13.event_loop/microtask_queue.js
+++ 13.event_loop/microtask_queue.js
@@ -5,7 +5,7 @@ const first = () => {
   console.log("first");
   setTimeout(second, 0);
   new Promise((resolve, reject) => {
-    console.log("I am right after third, before second");
+    resolve("I am right after third, before second");
   }).then((resolve) => console.log(resolve));
   third();
 };
