c62ba4c42214667c61871e1e27e8b54d032db32e
uzzal71 <uzzalroy.acm@gmail.com>
2024-03-18 09:04:44 +0600

    async-await

---
+++ 10.asynchronous/async-await.js
@@ -0,0 +1,27 @@
+const hasMeeting = false;
+
+const meeting = new Promise((resolve, reject) => {
+  if (!hasMeeting) {
+    const meetingDetails = {
+      name: "Technical Meeting",
+      localtion: "Google Meet",
+      time: "10:00 PM",
+    };
+    resolve(meetingDetails);
+  } else {
+    reject(new Error("Meeting already scheduled!"));
+  }
+});
+
+const addToCalendar = (meetingDetails) => {
+  const calendar = `${meetingDetails.name} has been scheduled on ${meetingDetails.localtion} at ${meetingDetails.time}`;
+  return Promise.resolve(calendar);
+};
+
+async function myMeeting() {
+  const meetingMetails = await meeting;
+  const calendar = await addToCalendar(meetingMetails);
+  console.log(calendar);
+}
+
+myMeeting();
