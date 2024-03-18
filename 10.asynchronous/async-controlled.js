4b08773c43dadd39b6fb8ec18bcdf6f2a391f805
uzzal71 <uzzalroy.acm@gmail.com>
2024-03-18 08:22:45 +0600

    async-controlled

---
+++ 10.asynchronous/async-controlled.js
@@ -0,0 +1,24 @@
+const takeOrder = (customer, callback) => {
+  console.log(`take order for ${customer}`);
+  callback(customer);
+};
+
+const processOrder = (customer, callback) => {
+  console.log(`processing order for ${customer}`);
+
+  setTimeout(() => {
+    console.log("cooking completed");
+    console.log(`order processed for ${customer}`);
+    callback(customer);
+  }, 3000);
+};
+
+const completeOrder = (customer) => {
+  console.log(`completed order for ${customer}`);
+};
+
+takeOrder("customer 1", (customer) => {
+  processOrder(customer, (customer) => {
+    completeOrder(customer);
+  });
+});
