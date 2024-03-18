1985e68ef931f509fd259536b144cc3c8d1a6e8c
uzzal71 <uzzalroy.acm@gmail.com>
2024-03-18 08:41:44 +0600

    promise

--- 10.asynchronous/promise.js
+++ 10.asynchronous/promise.js
@@ -1,1 +1,28 @@
-const promise = new Promise((resolve, reject) => {});
+const hasMeeting = false;
+
+const meeting = new Promise((resolve, reject) => {
+  if (!hasMeeting) {
+    const meetingDetails = {
+      name: "Technical Meeting",
+      localtion: "Google Meet",
+      time: "10:00 PM",
+    };
+    resolve(meetingDetails);
+  } else {
+    reject(new Error("Meeting already scheduled!"));
+  }
+});
+
+const addToCalendar = (meetingDetails) => {
+  const calendar = `${meetingDetails.name} has been scheduled on ${meetingDetails.localtion} at ${meetingDetails.time}`;
+  return Promise.resolve(calendar);
+};
+
+meeting
+  .then(addToCalendar)
+  .then((response) => {
+    console.log(response);
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
