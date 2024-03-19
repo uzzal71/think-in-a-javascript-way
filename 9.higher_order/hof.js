ee1a2e7a9ce14e9071745b9a34270152d3e501f1
uzzal71 <uzzalroy.acm@gmail.com>
2024-03-19 13:43:42 +0600

    higher order function

--- 9.higher_order/hof.js
+++ 9.higher_order/hof.js
@@ -1,3 +1,11 @@
 // higher order functions
 /// ফাংশনাল প্রোগ্রামিং হলো যেখানে আপনি এক বা একাধিক ফাংশনকে প্যারামিটার হিসেবে আর একটা ফাংশনে পাঠাতে পারবেন
 // একটা ফাংশন থেকে আর একটা ফাংশন রিটার্ন করা যায়।
+function hello() {
+  console.log("Hello World!");
+}
+
+hello();
+
+hello.language = "JavaScript";
+console.log(hello.language);
