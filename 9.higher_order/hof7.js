563b3b5f7ba710e53f7172b0b7713149e7efb703
uzzal71 <uzzalroy.acm@gmail.com>
2024-03-20 14:01:25 +0600

    higher order function

--- 9.higher_order/hof7.js
+++ 9.higher_order/hof7.js
@@ -1,1 +1,15 @@
 var languages = ["JavaScript", "Python", "PHP", "C"];
+
+function mapCustom(arr, fn) {
+  const newArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    newArray.push(fn(arr[i]));
+  }
+  return newArray;
+}
+
+console.log(
+  mapCustom(languages, function (language) {
+    return language.length;
+  })
+);
