52dfc5325ef8fadc1be318e3b35ff9e218f20337
uzzal71 <uzzalroy.acm@gmail.com>
2024-03-20 13:45:42 +0600

    higher order function

---
+++ 9.higher_order/hof6.js
@@ -0,0 +1,18 @@
+var players = [
+  {
+    name: "Sujon",
+    avg: 38.25,
+  },
+  {
+    name: "Uzzal",
+    avg: 44.33,
+  },
+  {
+    name: "Rahul",
+    avg: 35.18,
+  },
+  {
+    name: "Nasir",
+    avg: 52.14,
+  },
+];
