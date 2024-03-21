67803c9deafcde13bd1d546df6402529a77d0b64
uzzal71 <uzzalroy.acm@gmail.com>
2024-03-21 11:59:37 +0600

    prototype

--- 5.prototype/prototype2.js
+++ 5.prototype/prototype2.js
@@ -1,16 +1,23 @@
+const personMethods = {
+  eat() {
+    console.log(`Person is eating`);
+  },
+  sleep() {
+    console.log(`Person is sleeping`);
+  },
+  play() {
+    console.log(`Person is playing`);
+  },
+};
+
 function Person(name, age) {
   let person = {};
 
   person.name = name;
   person.age = age;
-
-  person.eat = function () {
-    console.log(`Person is eating`);
-  };
-
-  person.sleep = function () {
-    console.log(`Person is sleeping`);
-  };
+  person.eat = personMethods.eat;
+  person.sleep = personMethods.sleep;
+  person.play = personMethods.play;
 
   return person;
 }
