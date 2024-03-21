7aba705376c61d3256a7c25def2eb4f30d825364
uzzal71 <uzzalroy.acm@gmail.com>
2024-03-21 20:01:48 +0600

    Prototype Inheritance & ES6 Class

---
+++ 6.prototype-inheritance/classes4.js
@@ -0,0 +1,29 @@
+// Parent class
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
+
+// children class
+function Criketer(name, age, type, country) {
+  Person.call(this);
+  this.name = name;
+  this.age = age;
+  this.type = type;
+  this.country = country;
+}
+
+Person.prototype = {
+  eat: function () {
+    console.log(`${this.name} is eating`);
+  },
+};
+
+Criketer.prototype = Object.create(Person.prototype);
+Criketer.prototype.constructor = Criketer;
+Criketer.prototype.play = function () {
+  console.log(`${this.name} is playing`);
+};
+
+const sakib = new Criketer("Sakib", 35, "All rounder", "Bangladesh");
+console.log(sakib.play());
