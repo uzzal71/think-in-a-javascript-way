cb2149b09fa9b332dc5c925e5899a2b19eeeb093
uzzal71 <uzzalroy.acm@gmail.com>
2024-03-21 12:41:09 +0600

    prototype

--- 5.prototype/prototype5.js
+++ 5.prototype/prototype5.js
@@ -1,2 +1,24 @@
-function test() {}
-console.dir(test);
+const personMethods = {
+  eat() {
+    console.log(`Person is eating`);
+  },
+  sleep() {
+    console.log(`Person is sleeping`);
+  },
+  play() {
+    console.log(`Person is playing`);
+  },
+};
+
+function Person(name, age) {
+  let person = Object.create(personMethods);
+
+  person.name = name;
+  person.age = age;
+
+  return person;
+}
+
+const sakib = Person("Sakib", 35);
+const tamin = Person("Tamin", 35);
+tamin.play();
