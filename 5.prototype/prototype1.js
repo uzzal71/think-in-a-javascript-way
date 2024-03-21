4e7250ed4b8c001853d521ab4ed4ea1a685b7a27
uzzal71 <uzzalroy.acm@gmail.com>
2024-03-21 11:52:21 +0600

    prototype

---
+++ 5.prototype/prototype1.js
@@ -0,0 +1,12 @@
+let person = {};
+
+person.name = "Sujon";
+person.age = 30;
+
+person.eat = function () {
+  console.log(`Person is eating`);
+};
+
+person.sleep = function () {
+  console.log(`Person is sleeping`);
+};
