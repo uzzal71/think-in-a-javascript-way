81185721e6a19940e77ce0ef3352f590f48475ec
uzzal71 <uzzalroy.acm@gmail.com>
2024-03-21 19:09:15 +0600

    Prototype Inheritance & ES6 Class

--- 6.prototype-inheritance/classes1.js
+++ 6.prototype-inheritance/classes1.js
@@ -1,6 +1,15 @@
 function Person(name, age) {
-  person.name = name;
-  person.age = age;
+  this.name = name;
+  this.age = age;
 }
 
+Person.prototype = {
+  eat: function () {
+    console.log(`${this.name} is eating`);
+  },
+};
+
 const sakib = new Person("Sakib", 35);
+console.log(sakib);
+const tamim = new Person("Tamin", 35);
+console.log(tamim);
