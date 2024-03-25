6aad4073bd2f79c3678661aa8d0781859a20eb0f
uzzal71 <uzzalroy.acm@gmail.com>
2024-03-25 23:46:34 +0600

    assignment

--- express/index.js
+++ express/index.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
 app.listen(2000, () => {
   console.log("Server is running on port 2000");
 });
