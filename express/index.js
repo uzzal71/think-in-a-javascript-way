c0b6b9e7925f239eef5651b8c73662bb944791f1
uzzal71 <uzzalroy.acm@gmail.com>
2024-03-27 23:47:07 +0600

    test

--- express/index.js
+++ express/index.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/about", (req, res) => {
+  res.send("About Page");
+});
+
 app.listen(2000, () => {
   console.log("Server is running on port 2000");
 });
