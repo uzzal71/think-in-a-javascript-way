ee8a63835729cc240b2a01d7a6b76bd36de110a0
uzzal71 <uzzalroy.acm@gmail.com>
2024-07-05 23:56:50 +0600

    immutability

---
+++ 17.immutability_part_2/3.example.js
@@ -0,0 +1,17 @@
+let user = {
+    name: 'John Doe',
+    age: 25,
+    country: "USA",
+    city: "New York",
+    email: "john.doe@gmail.com",
+    phone: "123-456-7850",
+    address: "123 Main St.",
+    zip: "5456",
+    password: "1235479",
+    isAdmin: true
+}
+
+const obj = {name: "uzzal", address: "Dhaka"}
+const copyObj = {};
+Object.assign(copyObj, obj);
+console.log(obj);
