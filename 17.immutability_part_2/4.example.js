6439a531dfa75d802508de94915a36835d4543d5
uzzal71 <uzzalroy.acm@gmail.com>
2024-07-06 23:42:42 +0600

    immutability

--- 17.immutability_part_2/4.example.js
+++ 17.immutability_part_2/4.example.js
@@ -9,3 +9,14 @@ console.log(a === b); // true
 a.name = "uzzal";
 console.log(a);
 console.log(b);
+
+console.log(Object.is(a, b)); // true
+
+Object.seal(a);
+a.address = "Dhaka Bangladesh"; // seal not allowed
+console.log(a);
+
+delete a.name; // seal not allowed
+
+a.name = "Tapas"; // seal update allowed
+console.log(a);
