2bf8789426c57d6cd5a6237c7a208f764f3cb1bc
uzzal71 <uzzalroy.acm@gmail.com>
2024-07-07 00:05:39 +0600

    immutability

--- 17.immutability_part_2/6.deep_freeze.js
+++ 17.immutability_part_2/6.deep_freeze.js
@@ -9,18 +9,30 @@ let config = {
     },
 };
 
+// First working
+/*
 Object.freeze(config);
 config.db = "Oracle";
 console.log(config);
 config.admin.name = "sujon";
 console.log(config);
+*/
 
-let deepFreeze = () => {
+// Second working
+let deepFreeze = (obj) => {
     Object.keys(obj).forEach(key => {
         if (typeof obj[key] === 'object') {
             deepFreeze(obj[key]);
         } else {
-            Object.freeze(obj[key]);
+            Object.freeze(obj);
         }
     });
 };
+
+deepFreeze(config);
+// console.log(config);
+
+config.db = "Oracle";
+console.log(config);
+config.admin.name = "sujon";
+console.log(config);
