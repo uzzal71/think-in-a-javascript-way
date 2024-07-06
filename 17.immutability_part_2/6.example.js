fe3c9f24c2e6805a8746688de2b384c6b44463c8
uzzal71 <uzzalroy.acm@gmail.com>
2024-07-06 23:51:36 +0600

    immutability

---
+++ 17.immutability_part_2/6.example.js
@@ -0,0 +1,10 @@
+let config = {
+    "db": "postgresql",
+    "host": "localhost",
+    "password": "fake-password",
+    "port": 5432,
+    "admin": {
+        "name": "uzzal",
+        "rights": ["create", "update", "delete"]
+    },
+};
